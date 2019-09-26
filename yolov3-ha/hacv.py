18025ac2c0c8b271e4081b99a508c13f30041787
amlabs <28764847+andreas-amlabs@users.noreply.github.com>
2019-09-26 20:56:23 +0200

    MQTT fixes
    Optional MQTT authentication
    Optional MQTT paths from configuration

--- yolov3-ha/hacv.py
+++ yolov3-ha/hacv.py
@@ -27,16 +27,34 @@ class CVMQTTPlugin:
     timer = None
     name = "area"
     detects = {}
+    username = None
+    password = None
+    mqtt_tts = "ha/tts/mqtt"
+    mqtt_motion = "ha/motion/mqtt"
+    mqtt_camera = "ha/camera/mqtt"
 
     def __init__(self, cfg):
         if cfg is not None:
             broker_address = cfg['hacv']['host']
             self.name = cfg['hacv']['name']
+            if cfg['hacv']['username']:
+                self.username = cfg['hacv']['username']
+            if cfg['hacv']['password']:
+                self.password = cfg['hacv']['password']
+            if cfg['hacv']['mqtt_tts']:
+                self.mqtt_tts = cfg['hacv']['mqtt_tts']
+            if cfg['hacv']['mqtt_motion']:
+                self.mqtt_motion = cfg['hacv']['mqtt_motion']
+            if cfg['hacv']['mqtt_camera']:
+                self.mqtt_camera = cfg['hacv']['mqtt_camera']
         else:
             broker_address = "127.0.0.1"
+
         try:
             self.client = mqttClient.Client("Python-CV-YOLO3")
             self.client.on_connect = on_connect
+            if self.username and self.password:
+	        self.client.username_pw_set(self.username, self.password)
             self.client.connect(broker_address)
             self.client.loop_start()
         except:
@@ -47,20 +65,21 @@ class CVMQTTPlugin:
         if self.client == None:
             return
         debug("publishing motion OFF");
-        self.client.publish("ha/motion/mqtt", '{"on":"OFF"}')
+        self.client.publish(self.mqtt_motion, '{"on":"OFF"}')
 
     def publish_detection(self, detection_type, likelihood):
         if self.client == None:
             return
         debug("Publishing ", detection_type, likelihood)
+
         if detection_type not in self.detects:
             self.detects[detection_type] = 0
         if self.detects[detection_type] + 10.0 < time.time():
             self.detects[detection_type] = time.time()
             debug("publish TTS")
-            self.client.publish("ha/tts/say", "There is a " + detection_type + " in the " + self.name)
+            self.client.publish(self.mqtt_tts, "There is a " + detection_type + " in the " + self.name)
             debug("publish Motion")
-            self.client.publish("ha/motion/mqtt", '{"on":"ON", "type":"' + detection_type + '"}')
+            self.client.publish(self.mqtt_motion, '{"on":"ON", "type":"' + detection_type + '"}')
             if self.timer is not None:
                 self.timer.cancel()
             debug("Setting up timer for 15 seconds")
@@ -71,7 +90,7 @@ class CVMQTTPlugin:
         if self.client == None:
             return
         debug("Publishing image.")
-        self.client.publish("ha/camera/mqtt", image)
+        self.client.publish(self.mqtt_camera, image)
 
     def __del__(self):
         if self.client == None:

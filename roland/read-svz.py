1741dd0711d9a18f3c7a69746a6f3dea9a58ecaf
Joakim Eriksson <ruben.joakim.eriksson@gmail.com>
2022-01-03 12:40:52 +0100

    more metadata from file - and checksum

--- roland/read-svz.py
+++ roland/read-svz.py
@@ -6,15 +6,20 @@
 
 
 import sys
+import binascii
 
 # Offset from start
 
 startOffset = 128
 # Should setup the different parts for simpler offset calculations...
 # Check that the endian of word / long is correct also.
+
+
+
 patch = {
     'name':{'offset':8, 'len':16, 'type':'string'},
-    'level':{'offset':8 + 16 + 5, 'len':1, 'type':'int'},
+    'level':{'offset':28, 'len':1, 'type':'int'},
+    'pan':{'offset':29, 'len':1, 'type':'int'},
     'structure1-2':{'offset':1460, 'len':1, 'type':'int'},
     'structure3-4':{'offset':1461, 'len':1, 'type':'int'},
     'ring-level1-2':{'offset':1464, 'len':1, 'type':'int'},
@@ -88,12 +93,34 @@ for adr, b in enumerate(bytes_read):
 
 
 if bytes_read[0:3] == b'SVZ':
+    mlen = 0
+    plen = 0
+    pstart = 116
+    psize = 0
+    mstart = 0
     print("")
     print("Roland SVZ file")
+    if bytes_read[32:40] == b'PATaZCOR':
+        pstart = bytes_read[40] + bytes_read[41] * 256
+        plen = bytes_read[44] + bytes_read[45] * 256
+        print("  Type: Zen Core Patch Data of len: " + str(plen) + " starting at: " + str(pstart))
+    if bytes_read[48:56] == b'MDLaZCOR':
+        mstart = bytes_read[56] + bytes_read[57] * 256
+        mlen = bytes_read[60] + bytes_read[61] * 256
+        print("  Type: Zen Core MDL Data of len: " + str(mlen) + " starting at: " + str(mstart))
+    num_patches = bytes_read[pstart]
+    size_patches = bytes_read[pstart + 4] + bytes_read[pstart + 5] * 256
+    print("Number of patches in file:" + str(num_patches) + " size of patch: " + str(size_patches))
+    print("Total len:" + str(mlen + plen) + " Total file:" + str(adr + 1)) 
+    startOffset = 128 + num_patches * 4 - 4;
     print("First sound:", get_data('name', bytes_read))
     print("Level:" + str(get_data('level', bytes_read)))
+    print("Pan:" + str(get_data('pan', bytes_read)))
     print("Osc-mode-1-2:" + str(get_data('structure1-2', bytes_read)))
     print("Osc-mode-3-4:" + str(get_data('structure3-4', bytes_read)))
     print("Ring-level1-2:" + str(get_data('ring-level1-2', bytes_read)))
     print("Ring-level3-4:" + str(get_data('ring-level3-4', bytes_read)))
-
+    # Seems like the checksum is a plain CRC32 - which is great!
+    crc32 = binascii.crc32(bytes_read[128+12:1632+(128+12)])
+    checksum = bytes_read[128 + 4] + bytes_read[128 + 5] * 256 + bytes_read[128 + 6] * 65536 + bytes_read[128 + 7] * 16777216
+    print("CRC32: " + "%08x" % crc32 + " vs " + "%08x" % checksum)

5ca57bca8ded7436e2aeb448e5a2efdb4e909b28
Joakim Eriksson <joakim.eriksson@ri.se>
2022-04-19 22:13:47 +0200

    initial postit style poster and demo JS/HTML5 page

---
+++ js-postit/abstracts.js
@@ -0,0 +1,38 @@
+var abstracts =
+    [
+	{
+	    'title': 'Jamming Detection with JamSense ',
+	    'content': 'Low-power wireless networks transmit at low output power and are hence susceptible to cross-technology interference and jamming attacks. These may cause packet loss which may waste scarce energy resources by requiring the retransmission of packets. We present JamSense, a tool that is able to identify jamming attacks with high accuracy while not classifying Bluetooth or WiFi interference as jamming attacks. ',
+	    'link':'https://teams.microsoft.com/l/meetup-join/19%3ameeting_OTQ4MDE4NTAtNzU5Ny00N2UwLWIxYmMtMzhjOWI5NmMxMDA3%40thread.v2/0?context=%7b%22Tid%22%3a%225a9809cf-0bcb-413a-838a-09ecc40cc9e8%22%2c%22Oid%22%3a%22aff8612f-6661-4358-9445-2a150b3fa1e5%22%7d'
+	},
+	{
+	    'title': 'Data trace generation with Multi-Trace',
+	    'content': 'Wireless, low-power, multi-hop networks are exposed to numerous attacks also due to their resource constraints. While there has been a lot of work on intrusion detection systems for such networks, most of these studies have considered only a few topologies, scenarios, and attacks. One of the reasons for this shortcoming is the lack of sufficient data traces required to train many machine learning algorithms. In contrast to other wireless networks, multi-hop networks do not contain one entity that can capture all the traffic, making it more challenging to acquire such traces. We present Multi-Trace, an extension of the Cooja simulator with multi-level tracing facilities that enable data logging at different levels while maintaining a global time.',
+	    'link':'https://www.aftonbladet.se'
+	},
+	{
+	    'title': 'AI@Edge – AI for network automation',
+	    'content': 'The introduction of 5G technologies is a paradigm shift: its high performance in terms of latency, bitrate, and reliability, call for a technological and business convergence between the cloud computing and the telecom worlds. Features like edge computing, network slicing, and better and more flexible radio connectivity can be used to support qualitatively different applications.  Nevertheless, the challenges to be overcome in order to realize this connectivity/computing convergence are still notable. In particular, the increasing number of control and optimization dimensions of the end-to-end 5G infrastructure may result in an overly complex network that operators and vendors may find difficult to operate, manage, and evolve. The introduction of AI and Machine Learning (ML) technologies in the cloud-network convergence process will be crucial and help operators achieve a higher level of automation and increase network performance.  The aim of the AI@EDGE project is to build a platform and the tools that enable secure and automated roll-out of large-scale edge and cloud compute infrastructures, with close to zero-touch of the underlying heterogeneous MEC resources (network, storage, and compute resources). ',
+	    'link':'hejhej42'
+	},
+	{
+	    'title': '<title>',
+	    'content': '<abstract>',
+	    'link':'<the-teams-link'
+	},
+	{
+	    'title': '<title>',
+	    'content': '<abstract>',
+	    'link':'<the-teams-link'
+	},
+	{
+	    'title': '<title>',
+	    'content': '<abstract>',
+	    'link':'<the-teams-link'
+	},
+	{
+	    'title': '<title>',
+	    'content': '<abstract>',
+	    'link':'<the-teams-link'
+	},	
+    ]

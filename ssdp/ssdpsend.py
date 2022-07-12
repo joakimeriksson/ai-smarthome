8dd67d36d092f1236f01e758779cca1ed792553d
Joakim Eriksson <joakim.eriksson@ri.se>
2022-07-12 10:31:53 +0200

    update ssdp send

--- ssdp/ssdpsend.py
+++ ssdp/ssdpsend.py
@@ -1,6 +1,8 @@
+# Simplistic example sending SSDP M-Search on a specific interface.
+#
 import socket
-
-uUDP_IP = u'ff02::c%eno1'
+uIF = "eno1" # tun0
+uUDP_IP = u'ff02::c'
 iUDP_PORT = 1900
 uMessage = u'M-SEARCH * HTTP/1.1\r\nHOST: [%s]:%d\r\nMAN: "ssdp:discover"\r\nMX: 5\r\nST: %s\r\n\r\n' % (uUDP_IP, iUDP_PORT, "ssdp:all")
 
@@ -8,7 +10,6 @@ print("Sending:", uMessage)
 oSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 oSocket.settimeout(10)
 oSocket.setsockopt(socket.IPPROTO_IPV6, socket.IP_MULTICAST_TTL, 2)
+# Bind to specific interface...
+oSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, uIF.encode('utf-8'))
 oSocket.sendto(uMessage.encode('utf-8'), (uUDP_IP, iUDP_PORT))
-
-
-
